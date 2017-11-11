0d3d29055bd1d623001ae16d6f37bd86b68f1657
Greg Dubicki <gdubicki@egnyte.com>
2017-11-11 21:05:36 +0100

    Allow hookecho to exit with codes other than 0

--- test/hookecho.go
+++ test/hookecho.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func main() {
@@ -22,5 +23,15 @@ func main() {
 
 	if len(env) > 0 {
 		fmt.Printf("env: %s\n", strings.Join(env, " "))
+	}
+
+	if (len(os.Args) > 1) && (strings.HasPrefix(os.Args[1], "exit=")) {
+		exit_code_str := os.Args[1][5:]
+		exit_code, err := strconv.Atoi(exit_code_str)
+		if err != nil {
+			fmt.Printf("Exit code %s not an int!", exit_code_str)
+			os.Exit(-1)
+		}
+		os.Exit(exit_code)
 	}
 }
