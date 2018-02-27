495680c07e12ac74799e9308217e857c3d83affb
Hass_SEA <hassan@babaie.co.uk>
2018-02-26 16:28:57 -0800

    Update to Scalr Signature miss-match response
    Update to Scalr Signature miss-match response from ERROR to NIL. This is to resolve the issue with AND or OR Match rules

--- hook/hook.go
+++ hook/hook.go
@@ -146,8 +146,8 @@ func CheckScalrSignature(headers map[string]interface{}, body []byte, signingKey
  	expectedSignature := hex.EncodeToString(mac.Sum(nil))
  
  	if !hmac.Equal([]byte(providedSignature), []byte(expectedSignature)) {
- 		return false, &SignatureError{providedSignature}
- 	}
+ 		return false, nil
+	}
 
 	if !checkDate {
 		return true, nil
