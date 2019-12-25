c6c270c7ddd3d5850368c2273e78d30152f58111
Cameron Moore <moorereason@gmail.com>
2019-12-25 14:10:57 -0600

    Add SIGHUP support
    Fixes #352

--- signals.go
+++ signals.go
@@ -14,6 +14,7 @@ func setupSignals() {
 
 	signals = make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGUSR1)
+	signal.Notify(signals, syscall.SIGHUP)
 
 	go watchForSignals()
 }
@@ -23,11 +24,16 @@ func watchForSignals() {
 
 	for {
 		sig := <-signals
-		if sig == syscall.SIGUSR1 {
+		switch sig {
+		case syscall.SIGUSR1:
 			log.Println("caught USR1 signal")
 
 			reloadAllHooks()
-		} else {
+		case syscall.SIGHUP:
+			log.Println("caught HUP signal")
+
+			reloadAllHooks()
+		default:
 			log.Printf("caught unhandled signal %+v\n", sig)
 		}
 	}
